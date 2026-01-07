154c24799452e30167fee1b49546c693b6923f09
Calmu <huang_calvin@163.com>
2026-01-07 11:29:40 +0800

    hreflect 增加结构体和map的映射转换 & 修正EmbedCopy函数中src为指针时无法取字段的问题

---
+++ hreflect/embed_copy_test.go
@@ -0,0 +1,49 @@
+// Package hreflect
+//
+// ----------------develop info----------------
+//
+//	@Author xunmuhuang@rastar.com
+//	@DateTime 2026-1-7 10:18
+//
+// --------------------------------------------
+package hreflect
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStructToStruct(t *testing.T) {
+	type a struct {
+		Name string
+		Age  int
+	}
+	type b struct {
+		Name string
+		Age  int
+	}
+
+	tA := a{
+		Name: "mm",
+		Age:  18,
+	}
+	tB := b{}
+
+	fmt.Println(tA, tB)
+
+	EmbedCopy(&tB, &tA)
+
+	fmt.Println(tA, tB)
+
+	if tA.Name != tB.Name || tA.Age != tB.Age {
+		t.Error("StructToStruct error")
+	}
+
+	tC := a{Name: "mm2", Age: 19}
+	tD := b{}
+	fmt.Println(tC, tD)
+	EmbedCopy(&tD, tC)
+	fmt.Println(tC, tD)
+
+	t.Log("StructToStruct success")
+}
