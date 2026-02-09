54e47c443fffd41d86f8c7c8caed2e392e032a1f
Calmu <huang_calvin@163.com>
2026-02-09 10:06:49 +0800

    feat(streamload): - 增加重试请求机制

--- doris_streamload/stream_load.go
+++ doris_streamload/stream_load.go
@@ -5,12 +5,14 @@ import (
 	"compress/gzip"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"sync"
 	"time"
 
+	"github.com/avast/retry-go/v5"
 	"github.com/calmu/hgotool/hlog"
 	"go.uber.org/zap"
 )
@@ -128,92 +130,146 @@ func (s *StreamLoadClient) Load(data []byte) (*StreamLoadResponse, error) {
 
 	s.logger.Info("Starting StreamLoad request", zap.String("url", url), zap.Int("data_size", len(data)))
 
-	var body io.Reader
-	var contentEncoding string
-	// 处理压缩
-	if s.config.EnableGzip {
-		var buf bytes.Buffer
-		gz := gzip.NewWriter(&buf)
-		if _, err := gz.Write(data); err != nil {
-			s.logger.Error("Failed to compress data", zap.Error(err))
-			return nil, err
-		}
-		if err := gz.Close(); err != nil {
-			s.logger.Error("Failed to close gzip writer", zap.Error(err))
-			return nil, err
-		}
-		body = &buf
-		contentEncoding = "gzip"
-		s.logger.Info("Data compressed with gzip", zap.Int("original_size", len(data)), zap.Int("compressed_size", buf.Len()))
-	} else {
-		body = bytes.NewReader(data)
-	}
+	var resp *http.Response
+	var responseBody []byte
+	var streamLoadResp StreamLoadResponse
 
-	// 设置请求头
-	req, err := http.NewRequest("PUT", url, body)
-	if err != nil {
-		s.logger.Error("Failed to create HTTP request", zap.Error(err))
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Expect", "100-continue")
-	req.Header.Set("Authorization", "Basic "+basicAuth(s.config.Username, s.config.Password))
+	// 生成Label
+	var label, tmpLabel string
 	if s.config.Label != "" {
-		req.Header.Set("label", s.config.Label)
+		label = s.config.Label
 	} else {
 		// 自动生成label
-		label := fmt.Sprintf("%s%d", s.config.LabelPrefix, time.Now().Unix())
-		req.Header.Set("label", label)
+		label = fmt.Sprintf("%s%d", s.config.LabelPrefix, time.Now().Unix())
 		s.logger.Info("Generated stream load label", zap.String("label", label))
-	}
-	req.Header.Set("format", "json")
-	req.Header.Set("strip_outer_array", "true") // 支持数组格式的JSON数据
-
-	// 设置自定义头部参数
-	for headerName, headerValue := range s.config.Headers {
-		req.Header.Set(headerName, headerValue)
+		tmpLabel = label // 用来判断是否能重试时改label
 	}
 
-	if contentEncoding != "" {
-		req.Header.Set("Content-Encoding", contentEncoding)
-	}
+	// 最大重试次数
+	attempts := uint(3)
 
-	// 发送请求
-	resp, err := s.client.Do(req)
-	if err != nil {
-		s.logger.Error("Failed to send HTTP request", zap.Error(err))
-		return nil, err
-	}
-	defer resp.Body.Close()
+	err := retry.New(
+		retry.Attempts(attempts),
+		retry.Delay(time.Millisecond*100),
+		retry.DelayType(retry.RandomDelay),
+		retry.MaxDelay(time.Millisecond*800),
+		retry.MaxJitter(time.Millisecond*500),
+		retry.LastErrorOnly(true),
+		retry.OnRetry(func(retryNo uint, err error) {
+			s.logger.Info("StreamLoad request failed, retrying", zap.Error(err), zap.Uint("retry_no", retryNo))
+		}),
+		retry.RetryIf(func(err error) bool {
+			if err == nil {
+				return false
+			}
+			s.logger.Info("StreamLoad request failed, retrying", zap.Error(err), zap.Uint("attempts_left", attempts-1))
+			attempts--
+			return true
+		}),
+	).Do(func() error {
+		var body io.Reader
+		var contentEncoding string
+		// 处理压缩
+		if s.config.EnableGzip {
+			var buf bytes.Buffer
+			gz := gzip.NewWriter(&buf)
+			if _, err := gz.Write(data); err != nil {
+				s.logger.Info("Failed to compress data", zap.Error(err))
+				return fmt.Errorf("%w: write fail,%v", ErrCompressFail, err)
+			}
+			if err := gz.Close(); err != nil {
+				s.logger.Info("Failed to close gzip writer", zap.Error(err))
+				return fmt.Errorf("%w: close failed %v", ErrCompressFail, err)
+			}
+			body = &buf
+			contentEncoding = "gzip"
+			s.logger.Info("Data compressed with gzip", zap.Int("original_size", len(data)), zap.Int("compressed_size", buf.Len()))
+		} else {
+			body = bytes.NewReader(data)
+		}
 
-	// 读取响应
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		s.logger.Error("Failed to read response body", zap.Error(err))
-		return nil, err
-	}
+		// 设置请求头
+		req, err := http.NewRequest("PUT", url, body)
+		if err != nil {
+			s.logger.Info("Failed to create HTTP request", zap.Error(err))
+			return fmt.Errorf("%w: %v", ErrCreateRequestFail, err)
+		}
 
-	// 解析响应
-	var streamLoadResp StreamLoadResponse
-	if len(responseBody) > 0 {
-		if err := json.Unmarshal(responseBody, &streamLoadResp); err != nil {
-			s.logger.Warn("Failed to unmarshal response JSON, using default response", zap.Error(err), zap.String("response_body", string(responseBody)))
-			// 如果JSON解析失败，使用默认响应并记录警告
-			streamLoadResp = StreamLoadResponse{
-				Status:  "Success",
-				Label:   s.config.Label,
-				Message: fmt.Sprintf("Warning: Could not parse response JSON: %v", err),
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Expect", "100-continue")
+		req.Header.Set("Authorization", "Basic "+basicAuth(s.config.Username, s.config.Password))
+
+		req.Header.Set("label", label)
+
+		req.Header.Set("format", "json")
+		req.Header.Set("strip_outer_array", "true") // 支持数组格式的JSON数据
+
+		// 设置自定义头部参数
+		for headerName, headerValue := range s.config.Headers {
+			req.Header.Set(headerName, headerValue)
+		}
+
+		if contentEncoding != "" {
+			req.Header.Set("Content-Encoding", contentEncoding)
+		}
+
+		// 发送请求
+		resp, err = s.client.Do(req)
+		if err != nil {
+			s.logger.Info("Failed to send HTTP request", zap.Error(err))
+			return fmt.Errorf("%w: %v", ErrSendFail, err)
+		}
+		defer resp.Body.Close()
+
+		// 判断非正常状态码，此时应该重试
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("%w: need 200 got %d", ErrHttpStatusNotOk, resp.StatusCode)
+		} else {
+			// 读取响应
+			responseBody, err = io.ReadAll(resp.Body)
+			if err != nil {
+				s.logger.Info("Failed to read response body", zap.Error(err))
+				return fmt.Errorf("%w: %v", ErrReadResponseFail, err)
+			}
+			// 解析响应
+			if len(responseBody) > 0 {
+				if err = json.Unmarshal(responseBody, &streamLoadResp); err != nil {
+					s.logger.Info("Failed to unmarshal response JSON, using default response", zap.Error(err), zap.String("response_body", string(responseBody)))
+					return fmt.Errorf("%w: %v", ErrUnmarshalResponseFail, err)
+				}
+				// 增加判断，如果是返回信息说label重复，且attempts < 3，且tmpLabel==label， 则应给label加个后缀
+				if streamLoadResp.Status == "Label Already Exists" && attempts > 0 && tmpLabel == label {
+					label = fmt.Sprintf("%s_%d", label, attempts)
+					s.logger.Info("Label already exists, retrying with new label", zap.String("label", label))
+					return ErrReturnLabelAlreadyExist
+				}
+			} else {
+				s.logger.Info("Received empty response body, assuming success")
+				return ErrEmptyResponse
 			}
 		}
-	} else {
-		s.logger.Info("Received empty response body, assuming success")
+
+		return nil
+	})
+
+	if errors.Is(err, ErrUnmarshalResponseFail) {
+		// 如果JSON解析失败，使用默认响应并记录警告
+		streamLoadResp = StreamLoadResponse{
+			Status:  "Success",
+			Label:   s.config.Label,
+			Message: fmt.Sprintf("Warning: Could not parse response JSON: %v", err),
+		}
+	} else if errors.Is(err, ErrEmptyResponse) {
 		// 如果响应为空，假设成功并使用默认响应
 		streamLoadResp = StreamLoadResponse{
 			Status:  "Success",
 			Label:   s.config.Label,
 			Message: "Empty response received",
 		}
+	} else if err != nil {
+		// 因为retry包会重试，所以这里返回err，未走到streamLoadResp步骤
+		s.logger.Error("StreamLoad request failed", zap.Error(err))
+		return nil, err
 	}
 
 	s.logger.Info("StreamLoad response received", zap.String("status", streamLoadResp.Status), zap.String("label", streamLoadResp.Label), zap.Int64("load_bytes", streamLoadResp.LoadBytes))
@@ -221,8 +277,8 @@ func (s *StreamLoadClient) Load(data []byte) (*StreamLoadResponse, error) {
 	// 检查状态
 	// Doris StreamLoad 常见的状态码: 200(成功), 307(临时重定向到BE节点,此时如果是这个状态，则代表请求BE节点未通)
 	if resp.StatusCode != http.StatusOK {
-		s.logger.Warn("StreamLoad request failed", zap.Int("status_code", resp.StatusCode), zap.Any("headers", resp.Header), zap.String("response", string(responseBody)))
-		return &streamLoadResp, fmt.Errorf("request failed with status: %d, response: %s", resp.StatusCode, string(responseBody))
+		s.logger.Warn("StreamLoad request failed", zap.Int("status_code", resp.StatusCode), zap.Any("headers", resp.Header), zap.String("response", string(responseBody)), zap.Uint("attempts", attempts))
+		return &streamLoadResp, fmt.Errorf("%w: request failed with status: %d, response: %s", ErrHttpStatusNotOk, resp.StatusCode, string(responseBody))
 	} else {
 		s.logger.Info("StreamLoad request succeeded", zap.Int("status_code", resp.StatusCode), zap.String("status", streamLoadResp.Status), zap.String("label", streamLoadResp.Label))
 	}
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/avast/retry-go/v5 v5.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
